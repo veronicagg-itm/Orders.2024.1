a9cafa317788bb595834ec01b4b2229eb5cac402
Veronica Gaviria García <veronicagaviria1126235@correo.itm.edu.co>
2024-03-07 07:54:28 -0500

    Begin repository pattern

---
+++ Orders/Orders.Frontend/Repositories/HttpResponseWrapper.cs
@@ -0,0 +1,44 @@
+﻿namespace Orders.Frontend.Repositories
+{
+    public class HttpResponseWrapper<T>
+    {
+        public HttpResponseWrapper(T? response, bool error, HttpResponseMessage httpResponseMessage)
+        {
+            Response = response;
+            Error = error;
+            HttpResponseMessage = httpResponseMessage;
+        }
+
+        public T? Response { get; }    
+        public bool Error { get; }
+        public HttpResponseMessage HttpResponseMessage { get; }
+
+        public async Task<string?> GetErrorMessageAsync()
+        {
+            if (!Error) 
+            { 
+                return null;
+            }
+
+            var statusCode = HttpResponseMessage.StatusCode;
+            if (statusCode == System.Net.HttpStatusCode.NotFound) 
+            {
+                return "Recurso no encontrado.";
+            }
+            if (statusCode == System.Net.HttpStatusCode.BadRequest)
+            {
+                return await HttpResponseMessage.Content.ReadAsStringAsync();
+            }
+            if (statusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                return "Tienes que estar logueado para ejecutar esta operación.";
+            }
+            if (statusCode == System.Net.HttpStatusCode.Forbidden)
+            {
+                return "No tienes permisos para ejecutar esta operación.";
+            }
+
+            return "Ha ocurrido un error inesperado";
+        }
+    }
+}
