9c6dcec1f3e35a2d88a6adf2cf62acb369165d9e
Veronica Gaviria García <veronicagaviria1126235@correo.itm.edu.co>
2024-03-09 21:49:15 -0500

    pay management

--- Orders/Orders.Frontend/Program.cs
+++ Orders/Orders.Frontend/Program.cs
@@ -7,7 +7,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7247/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7247//") });
 builder.Services.AddScoped<IRepository, Repository>();
 
 await builder.Build().RunAsync();
