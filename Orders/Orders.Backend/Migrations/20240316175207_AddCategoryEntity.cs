05e8064cdc0e06d0a6da1bb515ab77bdbea00883
Veronica Gaviria García <veronicagaviria1126235@correo.itm.edu.co>
2024-03-16 18:45:08 -0500

    Repositories + Generetic Unit Work

---
+++ Orders/Orders.Backend/Migrations/20240316175207_AddCategoryEntity.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Orders.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoryEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Countries",
+                table: "Countries");
+
+            migrationBuilder.RenameTable(
+                name: "Countries",
+                newName: "Country");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Countries_Name",
+                table: "Country",
+                newName: "IX_Country_Name");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Country",
+                table: "Country",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Category_Name",
+                table: "Category",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Category");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Country",
+                table: "Country");
+
+            migrationBuilder.RenameTable(
+                name: "Country",
+                newName: "Countries");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Country_Name",
+                table: "Countries",
+                newName: "IX_Countries_Name");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Countries",
+                table: "Countries",
+                column: "Id");
+        }
+    }
+}
